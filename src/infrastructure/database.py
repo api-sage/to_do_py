ac6f52f4288b73361c01b711c6a11956c71d80f3
Paul Okikijesu Afolabi <paulafolabi745@gmail.com>
2025-08-18 18:07:39 +0100

    database created

--- src/infrastructure/database.py
+++ src/infrastructure/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQL_ALCHEMY_DATABASE_URI = "sqlite:///./todos.db"
+SQL_ALCHEMY_DATABASE_URI = "sqlite:///./src/infrastructure/todos.db"
 
 engine = create_engine(SQL_ALCHEMY_DATABASE_URI, connect_args={'check_same_thread': False})
 
